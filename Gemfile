395c29a7ab3b556125f588f0eed230e35b3a5ab4
Chris Heald <cheald@gmail.com>
2013-06-22 00:48:04 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem "mongo_mapper" #, :path => "/var/www/repos/mongomapper"
+gem "mongoid"
+gem "ruby-prof"
+gem "mongo"
+gem "bson_ext"
---
+++ mongo_orm_benchmark.rb
@@ -0,0 +1,89 @@
+require 'mongo'
+require 'bson'
+require 'mongo_mapper'
+require 'mongoid'
+require 'benchmark'
+require 'ruby-prof'
+
+m = Mongo::MongoClient.new
+m.drop_database "benchmark-mongomapper"
+m.drop_database "benchmark-mongoid"
+
+MongoMapper.setup({"production" => {"uri" => "mongodb://localhost/benchmark-mongomapper"}}, "production")
+Mongoid.load! File.expand_path("../mongoid.yml", __FILE__)
+Mongoid.identity_map_enabled = true
+
+BIRTHDAY = Time.at(123456789).to_date
+INSERTS = 1000
+LOOPS = 150
+
+class MMUser
+  include MongoMapper::Document
+  plugin MongoMapper::Plugins::IdentityMap
+
+  key :name,      String
+  key :age,       Integer
+  key :birthday,  Date
+  key :timestamp, Time
+end
+
+class MongoidUser
+  include Mongoid::Document
+
+  field :name,      type: String
+  field :age,       type: Integer
+  field :birthday,  type: Date
+  field :timestamp, type: Time
+end
+
+document = {name: "Chris", age: 30, birthday: BIRTHDAY, timestamp: Time.at(Time.now.to_i).utc}
+
+GC.start; GC.disable
+Benchmark.bm do |x|
+  x.report("MongoMapper Insert") { INSERTS.times { MMUser.create(document) } }
+  x.report("    Mongoid Insert") { INSERTS.times { MongoidUser.create(document) } }
+end
+
+# GC.start; GC.disable
+# MMUser.first.tap do |rec|
+#   document.each do |key, val|
+#     raise "Mismatch [MM]: Expected %s, got %s for %s" % [val, rec.send(key), key] unless rec.send(key) == val
+#   end
+# end
+#
+# GC.start; GC.disable
+# MongoidUser.first.tap do |rec|
+#   document.each do |key, val|
+#     raise "Mismatch [Mongoid]: Expected %s, got %s for %s" % [val, rec.send(key), key] unless rec.send(key) == val
+#   end
+# end
+#
+[1, 25, 100, 500, 1000].each do |batch|
+  GC.start; GC.disable
+  Benchmark.bm do |x|
+    x.report("MongoMapper Read #{batch}") { LOOPS.times { MMUser.limit(batch).to_a } }
+    x.report("    Mongoid Read #{batch}") { LOOPS.times { MongoidUser.limit(batch).to_a } }
+  end
+end
+
+#GC.start; GC.disable
+#Benchmark.bm do |x|
+#  x.report "Mongomapper" do
+#    result = RubyProf.profile do
+#      LOOPS.times { MMUser.limit(500).all }
+#    end
+#    open(File.expand_path("../callgrind.mm-prof", __FILE__), "w") do |f|
+#      RubyProf::CallTreePrinter.new(result).print(f, :min_percent => 1)
+#    end
+#  end
+#
+#  x.report "Mongoid" do
+#    result = RubyProf.profile do
+#      LOOPS.times { MongoidUser.limit(500).to_a }
+#    end
+#    open(File.expand_path("../callgrind.mongoid-prof", __FILE__), "w") do |f|
+#      RubyProf::CallTreePrinter.new(result).print(f, :min_percent => 1)
+#    end
+#  end
+#end
+#
